be76c8efee97356dbed7268670bbfd18e3bfdc8b
yuki-kimoto <kimoto.yuki@gmail.com>
2009-10-17 08:10:03 +0900

    add test

--- lib/DBI/Custom.pm
+++ lib/DBI/Custom.pm
@@ -1,14 +1,17 @@
 package DBI::Custom;
 use Object::Simple;
+
+our $VERSION = '0.0101';
+
+use Carp 'croak';
 use DBI;
-use SQL::Abstract;
 
 # Model
 sub model : ClassAttr { auto_build => \&_inherit_model }
 
 # Inherit super class model
 sub _inherit_model {
-    $class = shict;
+    my $class = shift;
     my $super = do {
         no strict 'refs';
         ${"${class}::ISA"}[0];
@@ -39,62 +42,35 @@ sub initialize_model {
 sub clone {
     my $self = shift;
     my $new = $self->Object::Simple::new;
-    $new->connect_infos(%{$self->connect_infos || {}});
+    $new->connect_info(%{$self->connect_info || {}});
     $new->filters(%{$self->filters || {}});
-    
-    $new->global_bind_rules(@{$self->global_bind_rules || []});
-    $new->global_fetch_rules(@{$self->global_fetch_rules || []});
-    
-    foreach my $method (qw/bind_rules fetch_rules/) {
-        my $new_rules = [];
-        foreach my $rule (@{$self->method}) {
-            my $new_rule = {};
-            foreach my $key ($rule) {
-                if ($key eq 'filter') {
-                    my $new_filters = [];
-                    foreach my $filter (@{$rule->{$key} || []}) {
-                        push @$new_filters, $filter
-                    }
-                    $new_rule->{$key} = $new_filters;
-                }
-                else {
-                     $new_rule->{$key} = $rule->{$key};
-                }
-            }
-            push @$new_rules, $new_rule;
-        }
-        $self->$method($new_rules);
-    }
+    $new->bind_filter($self->bind_filter);
+    $new->fetch_filter($self->fetch_filter);
 }
 
 # Attribute
 sub connect_info       : Attr { type => 'hash',  auto_build => sub { shift->connect_info({}) } }
 
-sub global_bind_rules  : Attr { type => 'array', auto_build => sub { shift->global_bind_rules([]) } }
-sub add_global_bind_rule { shift->global_bind_rules(@_) }
+sub bind_filter : Attr {}
+sub fetch_filter : Attr {}
 
-sub global_fetch_rules : Attr { type => 'array', auto_build => sub { shift->global_fetch_rules([]) } }
-sub add_global_fetch_rules { shift->global_fetch_rules(@_) }
-
-sub bind_rules : Attr { type => 'hash',  auto_build => sub { shift->bind_rules({}) }
-sub add_bind_rule { shift->bind_rules(@_) }
-
-sub fetch_rules : Attr { type => 'hash',  auto_build => sub { shift->fetch_rules({}) }
-sub add_fetch_rule { shift->fetch_rules(@_) }
-
-sub filters : Attr { type => 'hash', deref => 1, default => sub { {} } }
+sub filters : Attr { type => 'hash', deref => 1, auto_build => sub { shift->filters({}) } }
 sub add_filter { shift->filters(@_) }
 
-
 sub dbh          : Attr { auto_build => sub { shift->connect } }
-sub sql_abstract : Attr { auto_build => sub { shift->sql_abstract(SQL::Abstract->new) }}
+
+our %VALID_CONNECT_INFO = map {$_ => 1} qw/data_source user password options/;
 
 sub connect {
     my $self = shift;
     my $connect_info = $self->connect_info;
     
+    foreach my $key (keys %{$self->connect_info}) {
+        
+    }
+    
     my $dbh = DBI->connect(
-        $connect_info->{dsn},
+        $connect_info->{data_source},
         $connect_info->{user},
         $connect_info->{password},
         {
@@ -108,98 +84,9 @@ sub connect {
     $self->dbh($dbh);
 }
 
-sub reconnect {
-    my $self = shift;
-    $self->dbh(undef);
-    $self->connect;
-}
-
 sub query {
-    my ($self, $query, @binds) = @_;
-    $self->{success} = 0;
-
-    $self->_replace_omniholder(\$query, \@binds);
-
-    my $st;
-    my $sth;
-
-    my $old = $old_statements{$self};
-
-    if (my $i = (grep $old->[$_][0] eq $query, 0..$#$old)[0]) {
-        $st = splice(@$old, $i, 1)->[1];
-        $sth = $st->{sth};
-    } else {
-        eval { $sth = $self->{dbh}->prepare($query) } or do {
-            if ($@) {
-                $@ =~ s/ at \S+ line \d+\.\n\z//;
-                Carp::croak($@);
-            }
-            $self->{reason} = "Prepare failed ($DBI::errstr)";
-            return _dummy;
-        };
-
-        # $self is quoted on purpose, to pass along the stringified version,
-        # and avoid increasing reference count.
-        $st = bless {
-            db    => "$self",
-            sth   => $sth,
-            query => $query
-        }, 'DBIx::Simple::Statement';
-        $statements{$self}{$st} = $st;
-    }
-
-    eval { $sth->execute(@binds) } or do {
-        if ($@) {
-            $@ =~ s/ at \S+ line \d+\.\n\z//;
-            Carp::croak($@);
-        }
-
-        $self->{reason} = "Execute failed ($DBI::errstr)";
-	return _dummy;
-    };
-
-    $self->{success} = 1;
-
-    return bless { st => $st, lc_columns => $self->{lc_columns} }, $self->{result_class};
-}
-
-sub query {
-    my ($self, $sql) = @_;
-    my $sth = $self->dbh->prepare($sql);
-    $sth->execute(@bind);
-}
-
-sub select {
-    my ($table, $column_names, $where, $order) = @_;
     
-    my ($stmt, @bind) = $self->sql_abstract->select($table, $column_names, $where, $order);
-    my $sth = $self->dbh->prepare($stmt);
-    $sth->execute(@bind);
 }
-
-sub insert {
-    my ($self, $table, $values) = @_;
-    
-    my ($stmt, @bind) = $self->sql_abstract->insert($table, $values);
-    my $sth = $self->dbh->prepare($stmt);
-    $sth->execute(@bind);
-}
-
-sub update {
-    my ($self, $values, $where) = @_;
-    my ($stmt, @bind) = $self->sql_abstract->update($table, $values, $where);
-    my $sth = $self->dbh->prepare($stmt);
-    $sth->execute(@bind);
-}
-
-sub delete {
-    my ($self, $where) = @_;
-    my ($stmt, @bind) = $self->sql_abstract->delete($table, $where);
-    my $sth = $self->dbh->prepare($stmt);
-    $sth->execute(@bind);
-}
-
-
 
 Object::Simple->build_class;
 
@@ -209,44 +96,39 @@ DBI::Custom - The great new DBI::Custom!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0101
 
 =cut
-
-our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  my $dbi = DBI::Custom->new;
 
-Perhaps a little code snippet.
+=head1 METHODS
 
-    use DBI::Custom;
+=head2 add_filter
 
-    my $foo = DBI::Custom->new();
-    ...
+=head2 bind_filter
 
-=head1 EXPORT
+=head2 clone
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head2 connect
 
-=head1 FUNCTIONS
+=head2 connect_info
 
-=head2 function1
+=head2 dbh
 
-=cut
+=head2 fetch_filter
 
-sub function1 {
-}
+=head2 filters
 
-=head2 function2
+=head2 initialize_model
 
-=cut
+=head2 model
 
-sub function2 {
-}
+=head2 new
+
+=head2 query
 
 =head1 AUTHOR
 
---
+++ t/01-core.t
@@ -0,0 +1,176 @@
+use Test::More 'no_plan';
+use strict;
+use warnings;
+
+use DBI::Custom;
+use Scalar::Util qw/blessed/;
+
+# user password database
+our ($U, $P, $D) = connect_info();
+
+
+{
+    my $dbi = DBI::Custom->new(
+        connect_info => {
+            user => 'a',
+            password => 'b',
+            data_source => 'c',
+            options => {d => 1, e => 2}
+        },
+        filters => {
+            f => 3,
+        },
+        bind_filter => 'f',
+        fetch_filter => 'g',
+        dbh => 'e',
+    );
+    
+    is_deeply($dbi,{connect_info => {user => 'a', password => 'b', data_source => 'c', options => {d => 1, e => 2}}                       ,filters => {f => 3}, bind_filter => 'f', fetch_filter => 'g', dbh => 'e'}, 'new');
+    
+    isa_ok($dbi, 'DBI::Custom');
+}
+
+{
+    package DBI::Custom::T1;
+    use base 'DBI::Custom';
+    
+    __PACKAGE__->initialize_model(sub {
+        my $model = shift;
+        
+        $model
+          ->connect_info(
+            user => 'a',
+            password => 'b',
+            data_source => 'c',
+            options => {d => 1, e => 2}
+          )
+          ->filters(
+            f => 3
+          )
+          ->bind_filter('f')
+          ->fetch_filter('g')
+          ->dbh('e')
+    });
+}
+{
+    my $dbi = DBI::Custom::T1->new(
+        connect_info => {
+            user => 'ao',
+            password => 'bo',
+            data_source => 'co',
+            options => {do => 10, eo => 20}
+        },
+        filters => {
+            fo => 30,
+        },
+        bind_filter => 'fo',
+        fetch_filter => 'go',
+    );
+    
+    is_deeply($dbi,{connect_info => {user => 'ao', password => 'bo', data_source => 'co', options => {do => 10, eo => 20}}                       ,filters => {fo => 30}, bind_filter => 'fo', fetch_filter => 'go'}, 'new arguments');
+    
+    isa_ok($dbi, 'DBI::Custom::T1');
+}
+
+{
+    my $dbi = DBI::Custom::T1->new;
+    
+    is_deeply($dbi,{connect_info => {user => 'a', password => 'b', data_source => 'c', options => {d => 1, e => 2}}                       ,filters => {f => 3}, bind_filter => 'f', fetch_filter => 'g'}, 'new custom class');
+    
+    isa_ok($dbi, 'DBI::Custom::T1');
+    
+}
+
+{
+    package DBI::Custom::T1_2;
+    use base 'DBI::Custom::T1';
+}
+
+{
+    my $dbi = DBI::Custom::T1_2->new;
+    
+    is_deeply($dbi,{connect_info => {user => 'a', password => 'b', data_source => 'c', options => {d => 1, e => 2}}                       ,filters => {f => 3}, bind_filter => 'f', fetch_filter => 'g'}, 'new custom class inherit');
+    
+    isa_ok($dbi, 'DBI::Custom::T1_2');
+}
+
+{
+    package DBI::Custom::T1_3;
+    use base 'DBI::Custom::T1';
+    
+    __PACKAGE__->initialize_model(sub {
+        my $model = shift;
+        
+        $model
+          ->connect_info(
+            user => 'ao',
+            password => 'bo',
+            data_source => 'co',
+            options => {do => 10, eo => 20}
+          )
+          ->filters(
+            fo => 30
+          )
+          ->bind_filter('fo')
+          ->fetch_filter('go')
+          ->dbh('eo')
+    });
+    
+}
+
+{
+    my $dbi = DBI::Custom::T1_3->new;
+    
+    is_deeply($dbi,{connect_info => {user => 'ao', password => 'bo', data_source => 'co', options => {do => 10, eo => 20}}                       ,filters => {fo => 30}, bind_filter => 'fo', fetch_filter => 'go'}, 'new custom class');
+    
+    isa_ok($dbi, 'DBI::Custom::T1_3');
+}
+
+{
+    my $dbi = DBI::Custom::T1_3->new(
+        connect_info => {
+            user => 'a',
+            password => 'b',
+            data_source => 'c',
+            options => {d => 1, e => 2}
+        },
+        filters => {
+            f => 3,
+        },
+        bind_filter => 'f',
+        fetch_filter => 'g',
+        dbh => 'e',
+    );
+    
+    is_deeply($dbi,{connect_info => {user => 'a', password => 'b', data_source => 'c', options => {d => 1, e => 2}}                       ,filters => {f => 3}, bind_filter => 'f', fetch_filter => 'g', dbh => 'e'}, 'new');
+    
+    isa_ok($dbi, 'DBI::Custom');
+}
+
+{
+    my $dbi = DBI::Custom->new(
+        connect_info => {
+            user => $U,
+            password => $P,
+            data_source => "dbi:mysql:$D"
+        }
+    );
+    $dbi->connect;
+    
+    ok(blessed $dbi->dbh);
+    can_ok($dbi->dbh, qw/prepare/);
+    
+    
+}
+
+sub connect_info {
+    my $file = 'password.tmp';
+    open my $fh, '<', $file
+      or return;
+    
+    my ($user, $password, $database) = split(/\s/, (<$fh>)[0]);
+    
+    close $fh;
+    
+    return ($user, $password, $database);
+}
